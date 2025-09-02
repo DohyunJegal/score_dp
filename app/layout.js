315434c394c9aaba259b6468c23269ab4c8de934
DohyunJegal <dohyunm14@chungbuk.ac.kr>
2025-09-02 15:01:07 +0900

    added pwa

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,17 @@ import Navigation from './components/Navigation'
 export const metadata = {
   title: 'score.dp',
   description: 'iidx dp table',
+  manifest: '/manifest.json',
+  themeColor: '#000000',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'score.dp',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
